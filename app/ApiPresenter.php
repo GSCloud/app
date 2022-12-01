d9ee0b7d4ef392bd93b2884d44ff175f8fbf7af6
Filip Oščádal <f@mxd.cz>
2022-12-01 21:05:55 +0100

    minor update

--- app/ApiPresenter.php
+++ app/ApiPresenter.php
@@ -67,7 +67,7 @@ class ApiPresenter extends APresenter
         // general API properties
         $extras = [
             "fn" => $view,
-            "name" => "LAHVE REST API",
+            "name" => "APP REST API",
             "api_quota" => (int) self::MAX_API_HITS,
             "api_usage" => $this->accessLimiter(),
             "access_time_limit" => self::ACCESS_TIME_LIMIT,
